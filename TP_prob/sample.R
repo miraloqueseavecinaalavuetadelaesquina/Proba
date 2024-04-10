5eff45d5e119c90ad8de55b46fcfb7d89f28432e
caianto <105373685+miraloqueseavecinaalavuetadelaesquina@users.noreply.github.com>
2024-04-10 15:00:17 -0300

    funciones Ej 3, 6,7

--- TP_prob/sample.R
+++ TP_prob/sample.R
@@ -1,17 +1,93 @@
 # TP
-a <- sample(1:6,1, replace = T)
-print(a)
-for (i in 1:10){
-  print(sample(1:6, size = 5, replace = F))
-}
 
-s <- c()
+#***********
+#*Funciones
+#***********
 
-figus <- function(n){
-  s <- c()
-  c <- 0
-  while (c<n) {
-    ev <- sample(1:n,1)
-    
+# Ej 3
+cuantasFigus <- function(figusTotal){
+  # Generamos album vacio
+  album <- rep(T,times=figusTotal)
+  cantFiguras <- 0
+  vacias <- figusTotal
+  while (vacias>0) {
+    figura <- sample(1:figusTotal,1,replace = T)
+    if(album[figura]){album[figura] <- F} # figura encontrada
+    vacias <- sum(album)
+    cantFiguras <- cantFiguras + 1
   }
+  return(cantFiguras)
 }
+
+
+
+# Ej 7
+genPaquete <- function(figusTotal, figusPaquete){
+  return(sample(1:figusTotal,figusPaquete, replace = T))
+}
+
+# Ej 8
+cuantosPaquetes <- function(figusTotal, figusPaquete){
+  album <- rep(T,times=figusTotal)
+  cantPaquetes <- 0
+  vacias <- figusTotal
+  while (vacias>0) {
+    paquete <- genPaquete(figusTotal, figusPaquete)
+    for (fig in 1:figusPaquete){
+      if (album[paquete[fig]]){album[paquete[fig]] <- F} # figura encontrada
+    }
+    vacias = sum(album)
+    cantPaquetes <- cantPaquetes + 1
+  }
+  return(cantPaquetes)
+}
+
+simuFiguritas <- function(figusTotal, figusPaquete=1, Nrep=10, tipoSimu=F){
+  vec <- c()
+  if(tipoSimu){
+    for (i in 1:Nrep){
+      experimento <- cuantosPaquetes(figusTotal, figusPaquete)
+      vec[i] <- experimento
+    }
+  }else{
+    for (i in 1:Nrep){
+      experimento <- cuantasFigus(figusTotal)
+      vec[i] <- experimento
+    }
+  }
+  return(vec)
+}
+
+
+#***********
+#*Ejercicios
+#***********
+
+# Ej 1
+figurita <- sample(1:6,1)
+print(figurita[1])
+
+# Ej 2
+resultados <- cuantasFigus(figusTotal = 6)
+
+# Ej 4
+## a
+resultados <- simuFiguritas(figusTotal = 6,Nrep = 1000)
+media <- mean(resultados)
+print(paste("La cantidad de figuritas promedio para completar el album de 6 figuritas es : ", media))
+
+
+# Ej 5
+resultados <- simuFiguritas(figusTotal = 640,Nrep = 100)
+media <- mean(resultados)
+print(paste("La cantidad de figuritas promedio para completar el album de 6 figuritas es : ", media))
+
+# Ej 9
+resultados <- simuFiguritas(figusTotal = 640, figusPaquete = 5, Nrep = 100, tipoSimu = T)
+
+## a
+media <- mean(resultados)
+print(paste("La cantidad de figuritas promedio para completar el album de 6 figuritas es : ", media))
+
+
+
